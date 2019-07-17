d21d0b92c467e65dbfd801d79d20c42fbe61d858
zhaodoctor <zhaodoctor@users.noreply.github.com>
2019-07-17 09:57:40 +0800

    Update handlemodel2.pl

--- module/handlemodel2.pl
+++ module/handlemodel2.pl
@@ -22,27 +22,29 @@ while(<ID>){
 		my $str = $_;
 		chomp $str;
 		my $len = length($str);
-		if ($len <= $ws) {
-			print LAB $id,"\n";
-			print OUT $str,"\n";
-		} else {
-			my $st = 0;
-			my $m = 0;
-			while ($st + $ws < $len){
+		if ($len > 5){
+			if ($len <= $ws) {
+				print LAB $id,"\n";
+				print OUT $str,"\n";
+			} else {
+				my $st = 0;
+				my $m = 0;
+				while ($st + $ws < $len){
+					my $wd = substr($str,$st,$ws);
+					print LAB $id,"_";
+					print LAB $st+1,"_";
+					print LAB $st+$ws,"\n";
+					print OUT $wd,"\n";
+					$st = $st + $step;
+					$m++;
+				}
+				$st = $len - $ws;
 				my $wd = substr($str,$st,$ws);
-				print LAB $id,"_";
+        		        print LAB $id,"_";
 				print LAB $st+1,"_";
-				print LAB $st+$ws,"\n";
-				print OUT $wd,"\n";
-				$st = $st + $step;
-				$m++;
+				print LAB $len,"\n";
+        		        print OUT $wd,"\n";
 			}
-			$st = $len - $ws;
-			my $wd = substr($str,$st,$ws);
-        	        print LAB $id,"_";
-			print LAB $st+1,"_";
-			print LAB $len,"\n";
-        	        print OUT $wd,"\n";
 		}
 	}
 
